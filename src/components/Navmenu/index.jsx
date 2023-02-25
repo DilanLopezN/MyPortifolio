673c7784f506aa97249064f515f8bca30401cbda
Dilan Lopez <dilanlopez009@gmail.com>
2023-02-24 21:04:34 -0300

    test new degs

--- src/components/Navmenu/index.jsx
+++ src/components/Navmenu/index.jsx
@@ -1,28 +1,12 @@
 import { FlexContainer, ImgContainer, NavContainer } from './styles'
-import { IdentificationCard } from 'phosphor-react'
+import { IdentificationCard, FolderUser, AddressBook } from 'phosphor-react'
 import { NavLink } from 'react-router-dom'
 export function NavMenu() {
   return (
     <FlexContainer>
       <NavLink to="/">
-        <NavContainer height={120} width={120}>
-          <div className="line" />
-          <ImgContainer>
-            <IdentificationCard size={62} color="red" />
-          </ImgContainer>
-        </NavContainer>
-      </NavLink>
-
-      <NavLink to="/">
-        <NavContainer height={120} width={120}>
-          <div className="line" />
-          <ImgContainer>
-            <IdentificationCard size={62} color="red" />
-          </ImgContainer>
-        </NavContainer>
-      </NavLink>
-      <NavLink to="/">
-        <NavContainer height={120} width={120}>
+        <NavContainer marginTop={60} height={120} width={120}>
+          <div className="line2" />
           <ImgContainer>
             <IdentificationCard size={62} color="red" />
           </ImgContainer>
@@ -32,7 +16,17 @@ export function NavMenu() {
       <NavLink to="/">
         <NavContainer height={120} width={120}>
           <ImgContainer>
-            <IdentificationCard size={62} color="red" />
+            <AddressBook size={62} color="red" />
+          </ImgContainer>
+        </NavContainer>
+      </NavLink>
+
+      <NavLink to="/">
+        <NavContainer marginTop={60} height={120} width={120}>
+          <div className="line" />
+
+          <ImgContainer>
+            <FolderUser size={62} color="red" />
           </ImgContainer>
         </NavContainer>
       </NavLink>
